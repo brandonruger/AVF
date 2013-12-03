32b45bd8ddc18a8d6a5ec70614590bfa48fd3a86
r02548 <bruger@fullsail.edu>
2013-12-02 19:41:28 -0500

    Added custom CSS and JS files to both Android and iOS Apps.

---
+++ avfApp/platforms/ios/www/js/main.js
@@ -0,0 +1,10 @@
+/*Brandon Ruger
+ *AVF 1312*/
+
+$('#home').on('pageinit', function(){
+    //code needed for home page goes here
+});
+
+$('#research').on('pageinit', function(){
+    //code needed for research page goes here
+})
