f6cca460cc3e68d6fe08ad0558e03691810e17e4
r02548 <bruger@fullsail.edu>
2013-12-18 19:47:21 -0500

    Added compass page.

--- NewTestApp/platforms/ios/www/js/main.js
+++ NewTestApp/platforms/ios/www/js/main.js
@@ -70,53 +70,53 @@ var camError = function(errorMsg){
 
 //Weather API
 $("#viewweather").on("click", function(){
-            //URL to access Weather Source API.
-            //Weather key = 1cfe0a133d6e5228
-            var urlAdd = "http://api.wunderground.com/api/1cfe0a133d6e5228/conditions/q/33767.json";
+    //URL to access Weather Source API.
+    //Weather key = 1cfe0a133d6e5228
+    var urlAdd = "http://api.wunderground.com/api/1cfe0a133d6e5228/conditions/q/33767.json";
         
-            $.ajax({
-                url: urlAdd,
-                dataType: "jsonp",
-                success: function(forecast){
-                    console.log(forecast);
+    $.ajax({
+        url: urlAdd,
+        dataType: "jsonp",
+        success: function(forecast){
+            console.log(forecast);
                     
-                    var temp = "<li>Temperature: " + forecast.current_observation.temperature_string + "</li>";
-                    var feelsLike = "<li>Feels Like: " + forecast.current_observation.feelslike_string + "</li>";
-                    var heatIndex = "<li>Heat Index: " + forecast.current_observation.heat_index_string + "</li>";
-                    var precip = "<li>Precipiation: " + forecast.current_observation.precip_today_string + "</li>";
-                    var image = "<li><img src='" + forecast.current_observation.icon_url + "' alt='" + forecast.current_observation.icon + "' /></li>";
+            var temp = "<li>Temperature: " + forecast.current_observation.temperature_string + "</li>";
+            var feelsLike = "<li>Feels Like: " + forecast.current_observation.feelslike_string + "</li>";
+            var heatIndex = "<li>Heat Index: " + forecast.current_observation.heat_index_string + "</li>";
+            var precip = "<li>Precipiation: " + forecast.current_observation.precip_today_string + "</li>";
+            var image = "<li><img src='" + forecast.current_observation.icon_url + "' alt='" + forecast.current_observation.icon + "' /></li>";
                     
-                    $("#weatherdata").append(temp).append(feelsLike).append(heatIndex).append(precip).append(image);
+            $("#weatherdata").append(temp).append(feelsLike).append(heatIndex).append(precip).append(image);
                    
-                }
-            });
-        });
+        }
+    });
+});
 
 //Instagram API
 $("#viewphotos").on("click", function(){
-            //URL to access recent Instagram photos that are tagged with the word "beach".
-            var url = "https://api.instagram.com/v1/tags/clearwaterbeach/media/recent?callback=?&amp;client_id=18839eda02dc42e39ddfe9b7f77d1b61";
-            $.getJSON(url, getInstagramPhotos);
-        });
+    //URL to access recent Instagram photos that are tagged with the word "beach".
+    var url = "https://api.instagram.com/v1/tags/clearwaterbeach/media/recent?callback=?&amp;client_id=18839eda02dc42e39ddfe9b7f77d1b61";
+    $.getJSON(url, getInstagramPhotos);
+});
         
-        var getInstagramPhotos = function(feed) {
+var getInstagramPhotos = function(feed) {
             
-            console.log(feed); //To see what data I am getting back from Instagram.
+    console.log(feed); //To see what data I am getting back from Instagram.
             
-            //example HTML for pictures:
-            // <li><img src='{url}' alt='{caption}' /></li>
+    //example HTML for pictures:
+    // <li><img src='{url}' alt='{caption}' /></li>
             
-            $.each(feed.data, function(index, photodata){
-                console.log(photodata.images.standard_resolution.url); //Check to make sure targeting right URL's for photos.
-                var photos = "<li><img src='" + photodata.images.standard_resolution.url + "' alt='" + photodata.likes.count + "' /></li>";
+    $.each(feed.data, function(index, photodata){
+        console.log(photodata.images.standard_resolution.url); //Check to make sure targeting right URL's for photos.
+        var photos = "<li><img src='" + photodata.images.standard_resolution.url + "' alt='" + photodata.likes.count + "' /></li>";
                 
-                $("#instagramdata").append(photos); //Add downloaded photos into "instagramdata" ul.
+        $("#instagramdata").append(photos); //Add downloaded photos into "instagramdata" ul.
                 
-            }); //end each loop
+    }); //end each loop
             
             
             
-        }; //end getInstagramPhotos()
+}; //end getInstagramPhotos()
 
 
 //Device Ready Listener - will only work if I put this at the bottom of the page.
